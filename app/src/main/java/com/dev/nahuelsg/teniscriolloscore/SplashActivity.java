f7b6e63176f9b88f2d718984c15973642d6e72e6
Nahuel SG <nahuelsg_64@hotmail.com>
2017-02-08 13:45:02 -0300

    Splash screen

---
+++ app/src/main/java/com/dev/nahuelsg/teniscriolloscore/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.dev.nahuelsg.teniscriolloscore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nahuel SG on 08/02/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
