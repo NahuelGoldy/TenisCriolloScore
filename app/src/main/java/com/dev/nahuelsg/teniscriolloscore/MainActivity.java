6a3cf887b208d2aa119db93abccbab22043c88d3
Nahuel SG <nahuelsg_64@hotmail.com>
2017-02-11 00:23:14 -0300

    Ocultar menú de opciones para las actividades que no lo requieren

--- app/src/main/java/com/dev/nahuelsg/teniscriolloscore/MainActivity.java
+++ app/src/main/java/com/dev/nahuelsg/teniscriolloscore/MainActivity.java
@@ -82,6 +82,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+        menu.setGroupVisible(0,false);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
